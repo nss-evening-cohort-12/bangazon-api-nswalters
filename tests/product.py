0fb0906c26d0b55b9a36c972892f8d6d7b8a7006
Nick Walters <nswalters.s@gmail.com>
2021-02-11 18:54:40 -0600

    Added test which validates query_param accepts decimal price as well

--- tests/product.py
+++ tests/product.py
@@ -185,6 +185,16 @@ class ProductTests(APITestCase):
         self.assertEqual(len(json_response), 1)
         self.assertEqual(json_response[0]["id"], 1)
 
+        # Request products using decimal query param
+        url = "/products?min_price=14.99"
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.get(url, None, format='json')
+        json_response = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(json_response), 1)
+        self.assertEqual(json_response[0]["id"], 1)
+
     # TODO: Delete product
 
     # TODO: Product can be rated. Assert average rating exists.
