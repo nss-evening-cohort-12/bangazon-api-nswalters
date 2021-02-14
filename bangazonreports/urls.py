222b04e745baab1ec4544d1bbf8839b18d6ba429
Nick Walters <nswalters.s@gmail.com>
2021-02-13 20:56:15 -0600

    Added route and report for completed orders

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_expensive_products, list_inexpensive_products
+from .views import list_expensive_products, list_inexpensive_products, list_completed_orders
 
 urlpatterns = [
     path('reports/expensiveproducts', list_expensive_products),
     path('reports/inexpensiveproducts', list_inexpensive_products),
+    path('reports/completedorders', list_completed_orders),
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,3 +1,4 @@
 from .connection import Connection
 from .products.expensiveproducts import list_expensive_products
 from .products.inexpensiveproducts import list_inexpensive_products
+from .products.completedorders import list_completed_orders
