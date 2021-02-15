23bbbfd48965e34b48dad4c3a76005c6863c76dc
Nick Walters <nswalters.s@gmail.com>
2021-02-14 20:15:53 -0600

    Added '/reports/incompleteorders' route and report

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -2,3 +2,4 @@ from .connection import Connection
 from .products.expensiveproducts import list_expensive_products
 from .products.inexpensiveproducts import list_inexpensive_products
 from .products.completedorders import list_completed_orders
+from .products.incompleteorders import list_incomplete_orders
